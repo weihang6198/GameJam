e31038ec253753d9b8330158a394d02d131414df
Candle0216 <2240032@ecc.ac.jp>
2025-09-26 12:27:30 +0900

    9/26:　キーを押したときの画像表示

--- Assets/Scripts/InputRecorder.cs
+++ Assets/Scripts/InputRecorder.cs
@@ -1,15 +1,36 @@
 using NUnit.Framework;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InputRecorder : MonoBehaviour
 {
     private List<string> recordedKeyColors = new List<string>();
 
+    // UIimage参照
+    public Image upArrowImage;
+    public Image downArrowImage;
+    public Image leftArrowImage;
+    public Image rightArrowImage;
+
+    public Sprite KeyUp;
+    public Sprite KeyDown;
+    public Sprite KeyLeft;
+    public Sprite KeyRight;
+
+    public Transform UpPoint;
+    public Transform DownPoint;
+    public Transform LeftPoint;
+    public Transform rightPoint;
+
+    private Camera mainCamera;
+    private int KeyCount = 0;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        mainCamera = Camera.main;
+        ClearInputHistory();
     }
 
     // Update is called once per frame
@@ -19,19 +40,58 @@ public class InputRecorder : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             RecordInput("Red");
+            SetKeySpriteImage(upArrowImage, UpPoint, KeyUp);
+            KeyCount += 1;
+            Debug.Log("KeyCount:" + KeyCount);
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             RecordInput("Green");
+            SetKeySpriteImage(downArrowImage, DownPoint, KeyDown);
+            KeyCount += 1;
         }
         else if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             RecordInput("Blue");
+            SetKeySpriteImage(leftArrowImage, LeftPoint, KeyLeft);
+            KeyCount += 1;
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             RecordInput("Yellow");
+            SetKeySpriteImage(rightArrowImage, rightPoint, KeyRight);
+            KeyCount += 1;
         }
+
+        if(KeyCount >= 5)
+        {
+            HideAllImages();
+        }
+
+    }
+
+    // UIを変更するメソッド
+    private void SetKeySpriteImage(Image image, Transform displayPoint, Sprite sprite)
+    {
+        if(image != null && displayPoint != null && mainCamera != null)
+        {
+            // ワールド座標をスクリーン座標に変換してUIの位置を決める
+            Vector3 screenPos = mainCamera.WorldToScreenPoint(displayPoint.position);
+            image.rectTransform.position = screenPos;
+            image.sprite = sprite;
+            image.enabled = true;
+        }
+    }
+
+    // 画像を非表示にする
+    private void HideAllImages()
+    {
+        if(upArrowImage != null) upArrowImage.enabled = false;
+        if(downArrowImage != null) downArrowImage.enabled = false;
+        if(leftArrowImage != null) leftArrowImage.enabled = false;
+        if(rightArrowImage != null) rightArrowImage.enabled = false;
+
+        KeyCount = 0;
     }
 
     // 入力情報をリストに記憶
@@ -52,5 +112,6 @@ public class InputRecorder : MonoBehaviour
     {
         recordedKeyColors.Clear();
         Debug.Log("入力履歴をクリアしました");
+        HideAllImages();
     }
 }
